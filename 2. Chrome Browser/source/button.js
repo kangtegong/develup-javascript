8eabf4df51fba1578c83b8cabd61f3e37db2493d
Minchul Kang <tegongkang@gmail.com>
2020-04-01 21:45:33 +0900

    class

---
+++ 2. Chrome Browser/source/button.js
@@ -0,0 +1,14 @@
+function increaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+  }
+  
+  function decreaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('number').value = value;
+  }
