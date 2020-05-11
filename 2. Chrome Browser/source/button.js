aae8250e717ac0f6b9adcae73e8ab47779fa04cd
Minchul Kang <tegongkang@gmail.com>
2020-05-11 17:49:21 +0900

    lecture note

--- 2. Chrome Browser/source/button.js
+++ 2. Chrome Browser/source/button.js
@@ -3,12 +3,12 @@ function increaseValue() {
     value = isNaN(value) ? 0 : value;
     value++;
     document.getElementById('number').value = value;
-  }
-  
-  function decreaseValue() {
+}
+
+function decreaseValue() {
     var value = parseInt(document.getElementById('number').value, 10);
     value = isNaN(value) ? 0 : value;
     value < 1 ? value = 1 : '';
     value--;
     document.getElementById('number').value = value;
-  }
+}
